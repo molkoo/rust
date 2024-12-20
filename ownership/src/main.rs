48727061ff6f014e649bb96ad473ee0acf4f2054
Taeyoul Park <xoduf1447@gmail.com>
2024-12-20 17:00:02 +0900

    add ownership

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,15 +1,57 @@
 
+
 fn main() {
     let s1 = String::from("hello");
 
-    let (s2, len) = calulate_length(s1);
+    let (s2, len) = calculate_length(s1);
 
     println!("The length of '{}' is {}.", s2, len);
+
+    let s3 = String::from("world");
+
+    let len2 = calculate_length_new(&s3);
+
+    println!("The length of '{}' is {}", s3, len2);
+
+    let mut s4 = String::from("hello");
+
+    change(&mut s4);
+
+    println!("{}", s4);
+
+    let word = String::from("hello world");
+
+    //let first_word = first_word(&word);
+    let sword = "hello world";
+    let first_word = first_word(&sword);
+
+    println!("{}", first_word);
+
 }
 
 
-fn calulate_length(s: String) -> (String, usize) {
+fn calculate_length(s: String) -> (String, usize) {
     let length = s.len();
 
     (s, length)
+}
+
+fn calculate_length_new(s: &String) -> usize {
+    s.len()
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world!");
+}
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
 }
