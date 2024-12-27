53835b91eaa09672203494cf8bf6f6ca5c82025c
Taeyoul Park <xoduf1447@gmail.com>
2024-12-27 15:21:41 +0900

    struct end

--- struct_instance/src/main.rs
+++ struct_instance/src/main.rs
@@ -1,5 +1,3 @@
-use std::string;
-
 struct User {
     active: bool,
     username: String,
@@ -14,6 +12,15 @@ struct Point(i32, i32, i32);
 struct Rectangle {
     width: u32,
     height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 }
 
 fn main() {
@@ -48,9 +55,19 @@ fn main() {
         width: 30,
         height: 50,
     };
+    let rect2 = Rectangle {
+        width: 10,
+        height:40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height:45,
+    };
 
     println!("rect1 is {:?}", rect1);
-    println!("The area of rectangle is {} square pixels.", area(&rect1));
+    println!("The area of rectangle is {} square pixels.", rect1.area());
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
 
 fn build_user(email: String, username: String) -> User {
@@ -62,6 +79,6 @@ fn build_user(email: String, username: String) -> User {
     }
 }
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
-}
+// fn area(rectangle: &Rectangle) -> u32 {
+//     rectangle.width * rectangle.height
+// }
